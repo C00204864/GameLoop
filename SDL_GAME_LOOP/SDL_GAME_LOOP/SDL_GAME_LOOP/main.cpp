7b7045387d0000c3b71e4eff226952d36ec32898
C00204864 <C00204864@itcarlow.ie>
2019-02-01 11:56:58 +0000

    SDL + Box2D

---
+++ SDL_GAME_LOOP/SDL_GAME_LOOP/SDL_GAME_LOOP/main.cpp
@@ -0,0 +1,20 @@
+// SDL
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "SDL2main.lib")
+#pragma comment(lib, "SDL2test.lib")
+#pragma comment(lib, "image\\SDL2_image.lib")
+
+// Box2D
+#pragma comment(lib,"Box2D.lib")
+
+#include <Box2D/Box2D.h>
+
+#include "Game.h"
+
+int main(int argc, char* argv[])
+{
+	Game game;
+	game.run();
+
+	return 0;
+}
